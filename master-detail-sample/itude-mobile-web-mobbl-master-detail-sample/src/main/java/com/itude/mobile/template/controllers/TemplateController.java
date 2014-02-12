38ad125df27d63d47409943e0f32ad37e75c51de
Pjotter Tommassen <p.tommassen@itude.com>
2014-02-12 14:45:51 +0100

    - removed unnecessary comments

--- master-detail-sample/itude-mobile-web-mobbl-master-detail-sample/src/main/java/com/itude/mobile/template/controllers/TemplateController.java
+++ master-detail-sample/itude-mobile-web-mobbl-master-detail-sample/src/main/java/com/itude/mobile/template/controllers/TemplateController.java
@@ -42,21 +42,6 @@ public class TemplateController extends ApplicationController implements Seriali
   @Override
   public void initializeTab()
   {
-    // TODO: do something like this:
-    /*
-    String outcomeName = null;
-    if ("SHARES".equals(getView().getCurrentDialog())) outcomeName = "OUTCOME-tab_shares";
-    else if ("SEARCH".equals(getView().getCurrentDialog())) outcomeName = "OUTCOME-SearchAction";
-    else if ("ACCOUNT".equals(getView().getCurrentDialog())) outcomeName = "OUTCOME-tab_my_account";
-    else if ("HOME".equals(getView().getCurrentDialog())) outcomeName = "OUTCOME-tab_home";
-    else if ("INFO".equals(getView().getCurrentDialog())) outcomeName = "OUTCOME-page_info";
-
-    if (outcomeName != null)
-    {
-      MBOutcome outcome = new MBOutcome(outcomeName, getDataManagerService().loadDocument("MBEmptyDoc"));
-      handleOutcome(outcome);
-    }
-    */
   }
 
 }
